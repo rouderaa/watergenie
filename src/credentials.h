fc9e0271b61d4d3d0ffff6fde3f5abaa7023b0e3
rouderaa <rouderaa@hccnet.nl>
2025-10-28 12:42:58 +0100

    Replace WiFi credentials and add static IP config
    Updated WiFi credentials to placeholders and added static IP settings.

--- src/credentials.h
+++ src/credentials.h
@@ -1,3 +1,10 @@
 // WiFi credentials
-const char *ssid = "verweg";
-const char *password = "kindmango808";
+const char *ssid = "<yourwifissid>";
+const char *password = "<yourwifipassword>";
+
+// static IP settings
+IPAddress local_IP(192, 168, 21, 50); // My static IP->Use your ip range
+IPAddress gateway(192, 168, 21, 1);   // Router's IP ->Use your router IP
+IPAddress subnet(255, 255, 255, 0);
+IPAddress primaryDNS(192, 168, 21, 1); // Optional, not necessary 
+IPAddress secondaryDNS(192, 168, 21, 1);  // Optional, not necessary
